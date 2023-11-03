4140a3d6cc9e257fb70c04e57c4220b2aa075839
lilyob <lilyober07@gmail.com>
2023-11-03 15:51:38 -0700

    Work in progress on scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,24 @@
 
 
 window.onload = function () {
-    let form = document.querySelector("form")
+    let form = document.querySelector("form");
     window.addEventListener("submit", function (event) {
         event.preventDefault()
         const answer1 = document.querySelector("Morning").value;
         const answer2 = document.querySelector("Afternoon").value;
         const answer3 = document.querySelector("Night").value;
+
+        let results;
+
+        if (answer1 = "Morning") {
+            results = "Learn Python"
+        } else if (answer2 = "Afternoon") {
+            results = "Learn C#"
+        } else if (answer3 = "Night") {
+            results = "Learn JavaScript"
+        }
+
+        document.getElementById("results").innerText = results;
 
     });
 }
