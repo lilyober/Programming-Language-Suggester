c6910c220f9d190f752aa94c837aed90d7e19ff5
lilyob <lilyober07@gmail.com>
2023-11-03 16:39:12 -0700

    Work in progress on scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,41 @@
+// window.onload = function () {
+//     let form = document.querySelector("form");
+    document.addEventListener("submit", function(event) {
+        const Morning = document.getElementById("Morning");
+        Morning.addEventListener("submit", (event) {
+            const answer1 = document.getElementById("name").value;
 
-
-
-
-
-
-
-
-window.onload = function () {
-    let form = document.querySelector("form");
-    window.addEventListener("submit", function (event) {
+        document.getElementById("results").innerText = answer1;
         event.preventDefault()
-        const answer1 = document.querySelector("Morning").value;
-        const answer2 = document.querySelector("Afternoon").value;
-        const answer3 = document.querySelector("Night").value;
 
-        let results;
 
-        if (answer1 = "Morning") {
-            results = "Learn Python"
-        } else if (answer2 = "Afternoon") {
-            results = "Learn C#"
-        } else if (answer3 = "Night") {
-            results = "Learn JavaScript"
-        }
+        });
+        });
 
-        document.getElementById("results").innerText = results;
+//         event.preventDefault()
 
-    });
-}
+
+
+
+
+
+
+//         const answer1 = document.querySelector("Morning").value;
+//         const answer2 = document.querySelector("Afternoon").value;
+//         const answer3 = document.querySelector("Night").value;
+
+//         let results;
+
+//         if (answer1 = "Morning") {
+//             results = "Learn Python"
+//         } else if (answer2 = "Afternoon") {
+//             results = "Learn C#"
+//         } else if (answer3 = "Night") {
+//             results = "Learn JavaScript"
+//         }
+
+//         document.getElementById("results").innerText = results;
+ 
+//     });
+// }
 
