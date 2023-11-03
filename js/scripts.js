f9dc67cce37dd3544fe9b6efae35c10c7bc8ba1f
lilyob <lilyober07@gmail.com>
2023-11-03 15:20:14 -0700

    Initial work on scripts.js page.

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+
+window.onload = function () {
+    let form = document.querySelector("form")
+    window.addEventListener("submit", function (event) {
+        event.preventDefault()
+        const answer1 = document.querySelector("Morning").value;
+        const answer2 = document.querySelector("Afternoon").value;
+        const answer3 = document.querySelector("Night").value;
+
+    });
+}
+
