f5f1b530004d438ce1d3835f75eb6709f6d3bc4d
lilyob <lilyober07@gmail.com>
2023-11-04 11:59:02 -0700

    Add an event listener that responds to the button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,24 @@
+
+
+
+
+
+
+
+
+
+
+
 window.onload = function () {
-    let button = document.querySelector("button");
-    button.addEventListener("click", function(event) {
-        const Morning = document.getElementById("Morning");
-        Morning.addEventListener("submit", (event) => {
-            const answer1 = document.getElementById("name").value;
+    let form = document.querySelector("form")
+    form.addEventListener("submit", (event) => {
+        const morning = document.getElementById("Morning").value;
 
-        document.getElementById("results").innerText = answer1;
-        event.preventDefault()
+        document.querySelector("span#check").innerText = morning;
 
+        document.querySelector("div#story").removeAttribute("class");
 
-        });
-        });
-    };
-
-//         event.preventDefault()
-
-
-
-
-
-
-
-//         const answer1 = document.querySelector("Morning").value;
-//         const answer2 = document.querySelector("Afternoon").value;
-//         const answer3 = document.querySelector("Night").value;
-
-//         let results;
-
-//         if (answer1 = "Morning") {
-//             results = "Learn Python"
-//         } else if (answer2 = "Afternoon") {
-//             results = "Learn C#"
-//         } else if (answer3 = "Night") {
-//             results = "Learn JavaScript"
-//         }
-
-//         document.getElementById("results").innerText = results;
- 
-//     });
-// }
+        event.preventDefault();
+    });
+};
 
