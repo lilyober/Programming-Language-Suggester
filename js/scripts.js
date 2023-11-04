97563f469dcd08b65678b8d94f0a12629f848dfc
lilyob <lilyober07@gmail.com>
2023-11-04 13:29:52 -0700

    Add function to connect radio selection.

--- js/scripts.js
+++ js/scripts.js
@@ -2,23 +2,26 @@
 
 
 
+// "way to read the value connected"
+function theirChoice(event) {
+    event.preventDefault();
+    const timeSelection = document.querySelector("time").value.
+        window.addEventListener("load", function () {
+            document.getElementById("questions").addEventListener("submit", theirChoice);
+        });
+}
 
-// function theirChoice(event) {
-//     event.preventDefault
-//     const timeSelection = document.querySelector("time:checked").value.
-//     };
-
-
-// const timeSelection = document.querySelector("time:checked").value;
-
-
-
+// outputs "results" but is not connected to radio button
 window.onload = function () {
     let form = document.querySelector("form")
     form.addEventListener("submit", (event) => {
         const morning = document.getElementById("Morning").value;
+        const afternoon = document.getElementById("Afternoon").value;
+        const night = document.getElementById("Night").value;
 
         document.querySelector("span#check").innerText = morning;
+        document.querySelector("span#check2").innerText = afternoon;
+        document.querySelector("span#check3").innerText = night;
 
         document.querySelector("div#story").removeAttribute("class");
 
@@ -26,6 +29,7 @@ window.onload = function () {
     });
 };
 
+// BRANCH
 // if (timeSelection === "Morning") {
 //     document.querySelector("span#check").innerText = morning;
 // } else (timeSelection === "Afternoon"){
