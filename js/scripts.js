03ffdb14eaa10bdc8863ffb24afd3fc45018302f
lilyob <lilyober07@gmail.com>
2023-11-04 12:27:23 -0700

    Work in progress connecting radio selection to output.

--- js/scripts.js
+++ js/scripts.js
@@ -10,11 +10,31 @@
 
 
 window.onload = function () {
+    // function theirChoice(event) {
+    //     event.preventDefault
+    //     const timeSelection = document.querySelector("time:checked").value.
+    // }
+
+
     let form = document.querySelector("form")
     form.addEventListener("submit", (event) => {
-        const morning = document.getElementById("Morning").value;
+        const timeSelection = document.querySelector("time:checked").value.
 
-        document.querySelector("span#check").innerText = morning;
+        const morning = document.getElementById("Morning").value;
+        const afternoon = document.getElementById("Afternoon").value;
+        const night = document.getElementById("Night").value;
+
+        if (timeSelection === "Morning") {
+            document.querySelector("span#check").innerText = morning;
+            // document.querySelector("span#check2").removeAttribute("C#");
+
+        } else if (timeSelection === "Afternoon") {
+            document.querySelector("span#check2").innerText = afternoon;
+        } else if (timeSelection === "Night") {
+            document.querySelector("span#check3").innerText = night;
+        } else {
+            "Error"
+        }
 
         document.querySelector("div#story").removeAttribute("class");
 
