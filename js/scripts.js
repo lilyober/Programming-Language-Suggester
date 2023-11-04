780b0c4a7c4867e9bf2160c3a4a4de54bbad75ce
lilyob <lilyober07@gmail.com>
2023-11-04 13:02:19 -0700

    Work in progress on scripts/js.

--- js/scripts.js
+++ js/scripts.js
@@ -3,38 +3,22 @@
 
 
 
+// function theirChoice(event) {
+//     event.preventDefault
+//     const timeSelection = document.querySelector("time:checked").value.
+//     };
 
 
-
+// const timeSelection = document.querySelector("time:checked").value;
 
 
 
 window.onload = function () {
-    // function theirChoice(event) {
-    //     event.preventDefault
-    //     const timeSelection = document.querySelector("time:checked").value.
-    // }
-
-
     let form = document.querySelector("form")
     form.addEventListener("submit", (event) => {
-        const timeSelection = document.querySelector("time:checked").value.
-
         const morning = document.getElementById("Morning").value;
-        const afternoon = document.getElementById("Afternoon").value;
-        const night = document.getElementById("Night").value;
 
-        if (timeSelection === "Morning") {
-            document.querySelector("span#check").innerText = morning;
-            // document.querySelector("span#check2").removeAttribute("C#");
-
-        } else if (timeSelection === "Afternoon") {
-            document.querySelector("span#check2").innerText = afternoon;
-        } else if (timeSelection === "Night") {
-            document.querySelector("span#check3").innerText = night;
-        } else {
-            "Error"
-        }
+        document.querySelector("span#check").innerText = morning;
 
         document.querySelector("div#story").removeAttribute("class");
 
@@ -42,3 +26,20 @@ window.onload = function () {
     });
 };
 
+// if (timeSelection === "Morning") {
+//     document.querySelector("span#check").innerText = morning;
+// } else (timeSelection === "Afternoon"){
+//     document.querySelector("span#check2").innerText = afternoon;
+// } else if (timeSelection === "Night") {
+//     document.querySelector("span#check3").innerText = night;
+// };
+
+// if (morning === "Morning") {
+//     document.querySelector("span#check").innerText = morning;
+// } else if (afternoon === "Afternoon") {
+//     document.querySelector("span#check2").innerText = afternoon;
+// } else if (night === "Night") {
+//     document.querySelector("span#check3").innerText = night;
+// } else {
+//     "Error"
+// }
